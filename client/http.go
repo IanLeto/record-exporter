47a696b58c148757832e2ccfb96a539c226e8bf5
ianleto <ccssliu@163.com>
2024-07-23 21:13:39 +0800

    x

--- client/http.go
+++ client/http.go
@@ -1,1 +1,17 @@
 package client
+
+import (
+	"github.com/go-resty/resty/v2"
+	"sync"
+)
+
+var once sync.Once
+
+var client *resty.Client
+
+var GetClient = func() *resty.Client {
+	once.Do(func() {
+		client = resty.New()
+	})
+	return client
+}
